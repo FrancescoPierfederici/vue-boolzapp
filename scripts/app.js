cdf6b6ecc8191a4e4a7a2094f6be4bc66b0db70c
FrancescoPierfederici <francescopierf@gmail.com>
2021-05-05 15:19:54 +0200

    prima parte vue

--- scripts/app.js
+++ scripts/app.js
@@ -2,5 +2,12 @@ const app = new Vue({
     el: "#app",
     data: {
         usersList: globalUsersList,
+        activeChat : 0,
+        
+    },
+    methods:{
+        activeChatTwo(){
+
+        }
     }
 })
